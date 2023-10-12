4c707a86fb011aba98e888e901b5758292d9575d
D9J9V <macbookair@MACOS-D9J9V.local>
2023-10-12 13:07:12 -0600

    APIs Fucked up

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -1,19 +1,15 @@
-'use client'
+"use client";
 
-import { Session } from "next-auth"
-import { SessionProvider as Provider } from "next-auth/react"
+import { Session } from "next-auth";
+import { SessionProvider as Provider } from "next-auth/react";
 
 type Props = {
   children: React.ReactNode;
   session: Session | null;
+};
+
+function SessionProvider({ children, session }: Props) {
+  return <Provider>{children}</Provider>;
 }
 
-function SessionProvider({children, session}: Props) {
-  return (
-    <Provider>
-        {children}
-    </Provider>
-  )
-}
-
-export default SessionProvider
+export default SessionProvider;
