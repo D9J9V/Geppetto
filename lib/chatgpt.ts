e4b307c99c3654bdbcb9a79a4f657e741a9eabdd
D9J9V <macbookair@MACOS-D9J9V.local>
2023-10-12 00:35:07 -0600

    Migrated to OpenAI v4 via Grit

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,9 +1,9 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
-const configuration = new Configuration({
+
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(configuration);
 
 export default openai;
